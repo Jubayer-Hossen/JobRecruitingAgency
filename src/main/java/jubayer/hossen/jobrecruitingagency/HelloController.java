b5c624e22d98f2f377060b20ddf3689d5cb7f4f9
Jubayer-Hossen <136719793+Jubayer-Hossen@users.noreply.github.com>
2025-07-28 04:19:33 +0600

    Initial commit

---
+++ src/main/java/jubayer/hossen/jobrecruitingagency/HelloController.java
@@ -0,0 +1,14 @@
+package jubayer.hossen.jobrecruitingagency;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module jubayer.hossen.jobrecruitingagency {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens jubayer.hossen.jobrecruitingagency to javafx.fxml;
+    exports jubayer.hossen.jobrecruitingagency;
+}
