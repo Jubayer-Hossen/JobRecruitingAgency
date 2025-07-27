ac400a5502b4845b37539635e5a0afa439e745ef
Jubayer-Hossen <136719793+Jubayer-Hossen@users.noreply.github.com>
2025-07-28 05:01:59 +0600

    Add login page and controller

---
+++ src/main/java/jubayer/hossen/jobrecruitingagency/LoginController.java
@@ -0,0 +1,28 @@
+package jubayer.hossen.jobrecruitingagency;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private CheckBox rememberMECheckBox;
+    @javafx.fxml.FXML
+    private PasswordField passwordPasswordField;
+    @javafx.fxml.FXML
+    private TextField emailAddressOrUsernameTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void createAccountButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logInButtonOnAction(ActionEvent actionEvent) {
+    }
+}
