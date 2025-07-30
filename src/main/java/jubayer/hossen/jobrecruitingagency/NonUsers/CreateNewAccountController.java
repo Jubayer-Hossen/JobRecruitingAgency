27a92f27125d0900d8023f02493df73697f7e9fa
Jubayer-Hossen <136719793+Jubayer-Hossen@users.noreply.github.com>
2025-07-30 07:49:46 +0600

    Create 'Create new account portal'.

---
+++ src/main/java/jubayer/hossen/jobrecruitingagency/NonUsers/CreateNewAccountController.java
@@ -0,0 +1,7 @@
+package jubayer.hossen.jobrecruitingagency.NonUsers;
+
+public class CreateNewAccountController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
--- src/main/java/jubayer/hossen/jobrecruitingagency/NonUsers/LoginController.java
+++ src/main/java/jubayer/hossen/jobrecruitingagency/NonUsers/LoginController.java
@@ -1,9 +1,14 @@
 package jubayer.hossen.jobrecruitingagency.NonUsers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import jubayer.hossen.jobrecruitingagency.MainApplication;
 
 public class LoginController
 {
@@ -13,6 +18,8 @@ public class LoginController
     private PasswordField passwordPasswordField;
     @javafx.fxml.FXML
     private TextField emailAddressOrUsernameTextField;
+    @javafx.fxml.FXML
+    private Button createAccountButton;
 
     @javafx.fxml.FXML
     public void initialize() {
@@ -20,6 +27,16 @@ public class LoginController
 
     @javafx.fxml.FXML
     public void createAccountButtonOnAction(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("NonUsers/CreateNewAccountView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage newStage = (Stage) createAccountButton.getScene().getWindow();
+            newStage.setTitle("New Account Registration");
+            newStage.setScene(scene);
+            newStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @javafx.fxml.FXML
