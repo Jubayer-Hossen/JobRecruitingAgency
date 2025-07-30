05c42364789d6a38c4f6ffa1dcdcfbd0931f89dc
Jubayer-Hossen <136719793+Jubayer-Hossen@users.noreply.github.com>
2025-07-31 03:44:26 +0600

    Add profileView

--- src/main/java/jubayer/hossen/jobrecruitingagency/JobSeeker/Application.java
+++ src/main/java/jubayer/hossen/jobrecruitingagency/JobSeeker/Application.java
@@ -1,5 +1,79 @@
 package jubayer.hossen.jobrecruitingagency.JobSeeker;
 
-public class Application {
+import jubayer.hossen.jobrecruitingagency.BusinessDevelopmentExecutive.Company;
 
+public class Application {
+    private String applicationID;
+    private JobSeeker jobSeeker;
+    private Company company;
+    private Job job;
+    private String status;
+    private String applicationDate;
+
+    public Application(String applicationDate, String applicationID, Company company,
+                       Job job, JobSeeker jobSeeker, String status) {
+        this.applicationDate = applicationDate;
+        this.applicationID = applicationID;
+        this.company = company;
+        this.job = job;
+        this.jobSeeker = jobSeeker;
+        this.status = status;
+    }
+
+    public boolean submit(){
+        //
+        return true;
+    }
+
+    public String viewStatus(){
+        return status;
+    }
+
+    public String getApplicationDate() {
+        return applicationDate;
+    }
+
+    public void setApplicationDate(String applicationDate) {
+        this.applicationDate = applicationDate;
+    }
+
+    public String getApplicationID() {
+        return applicationID;
+    }
+
+    public void setApplicationID(String applicationID) {
+        this.applicationID = applicationID;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    public JobSeeker getJobSeeker() {
+        return jobSeeker;
+    }
+
+    public void setJobSeeker(JobSeeker jobSeeker) {
+        this.jobSeeker = jobSeeker;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
---
+++ src/main/java/jubayer/hossen/jobrecruitingagency/JobSeeker/JobSeekerProfileController.java
@@ -0,0 +1,7 @@
+package jubayer.hossen.jobrecruitingagency.JobSeeker;
+
+public class JobSeekerProfileController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
